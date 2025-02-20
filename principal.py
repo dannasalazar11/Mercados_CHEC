087227d1daa92dee938854dd91a6b34b471f79e2
dannasalazar11 <dsalazard@unal.edu.co>
2025-02-20 16:32:10 -0500

    act

--- principal.py
+++ principal.py
@@ -2,7 +2,7 @@ import streamlit as st
 from secciones import inicio, lineales, no_lineales, tabnet, propuesta  # Importar las secciones de la carpeta "pages"
 
 st.sidebar.title("Índice")
-sections = ["Inicio", "Regresores Clásicos Lineales", "Regresores Clásicos No Lineales", "TabNet" "Propuesta"]
+sections = ["Inicio", "Regresores Clásicos Lineales", "Regresores Clásicos No Lineales", "TabNet", "Propuesta"]
 choice = st.sidebar.radio("Selecciona una sección", sections)
 
 if choice == "Inicio":

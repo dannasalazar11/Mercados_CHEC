79822042b45a2a76620ff65cd589fe4541f60fe8
dannasalazar11 <dsalazard@unal.edu.co>
2025-02-20 15:23:34 -0500

    act

--- principal.py
+++ principal.py
@@ -1,15 +1,18 @@
 import streamlit as st
-from secciones import inicio, lineales, propuesta  # Importar las secciones de la carpeta "pages"
+from secciones import inicio, lineales, no_lineales, propuesta  # Importar las secciones de la carpeta "pages"
 
 st.sidebar.title("Índice")
-sections = ["Inicio", "Regresores Clásicos", "Propuesta"]
+sections = ["Inicio", "Regresores Clásicos Lineales", "Regresores Clásicos No Lineales", "Propuesta"]
 choice = st.sidebar.radio("Selecciona una sección", sections)
 
 if choice == "Inicio":
     inicio.mostrar()
 
-elif choice == "Regresores Clásicos":
+elif choice == "Regresores Clásicos Lineales":
     lineales.mostrar()
+
+elif choice == "Regresores Clásicos No Lineales":
+    no_lineales.mostrar()
 
 elif choice == "Propuesta":
     propuesta.mostrar()

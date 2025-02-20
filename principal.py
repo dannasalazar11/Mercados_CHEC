bbe53b1effd976ba2d0bbee1c18ad3beb6f1089b
dannasalazar11 <dsalazard@unal.edu.co>
2025-02-20 14:59:43 -0500

    act

--- principal.py
+++ principal.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from pages import inicio, lineales, propuesta  # Importar las secciones de la carpeta "pages"
 
-st.set_page_config(page_title="Navegación por secciones", layout="wide")
+# st.set_page_config(page_title="Navegación por secciones", layout="wide")
 
 st.sidebar.title("Índice")
 sections = ["Inicio", "Regresores Clásicos", "Propuesta"]

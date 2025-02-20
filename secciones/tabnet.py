cfdd04b82eb9b228d116ab2774354fc5430292ab
dannasalazar11 <dsalazard@unal.edu.co>
2025-02-20 16:31:44 -0500

    act

--- secciones/tabnet.py
+++ secciones/tabnet.py
@@ -3,6 +3,8 @@ import streamlit as st
 def mostrar():
     st.title("TabNet")
 
-# Mostrar una imagen desde una ruta local
-st.image("Datos/Imagenes/tabnet.jpg", caption="Descripción de la imagen", use_column_width=True)
+    # Mostrar una imagen desde una ruta local
+    st.image("Datos/Imagenes/tabnet.jpg", caption="Descripción de la imagen", use_container_width=True)
+
+
   

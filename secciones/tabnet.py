909b78f8a3b5eab711efecff8716c88463d77e6b
dannasalazar11 <dsalazard@unal.edu.co>
2025-02-20 16:49:35 -0500

    act

--- secciones/tabnet.py
+++ secciones/tabnet.py
@@ -2,6 +2,32 @@ import streamlit as st
 import torch
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
+from pytorch_tabnet.tab_model import TabNetRegressor
+import numpy as np
+
+def my_r2_score_fn(y_pred, y_true):
+    total_variance = torch.var(y_true, unbiased=False)
+    unexplained_variance = torch.mean((y_true - y_pred) ** 2)
+    r2_score = unexplained_variance / total_variance
+    return r2_score
+
+class CustomTabNetRegressor(TabNetRegressor):
+    def __init__(self, *args, **kwargs):
+        super(CustomTabNetRegressor, self).__init__(*args, **kwargs)
+
+    def forward(self, X):
+        output, M_loss = self.network(X)
+        output = torch.relu(output)
+        return output, M_loss
+
+    def predict(self, X):
+        device = next(self.network.parameters()).device
+        if not isinstance(X, torch.Tensor):
+            X = torch.tensor(X, dtype=torch.float32)
+        X = X.to(device)
+        with torch.no_grad():
+            output, _ = self.forward(X)
+        return output.cpu().numpy()
 
 # Cargar el modelo
 @st.cache_resource

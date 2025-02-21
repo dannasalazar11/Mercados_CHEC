a97f1901761de4ed46c64a98e5b72f6a986546a8
dannasalazar11 <dsalazard@unal.edu.co>
2025-02-20 19:40:22 -0500

    .

--- secciones/tabnet.py
+++ secciones/tabnet.py
@@ -36,42 +36,42 @@ def load_model():
     model.eval()  # Poner en modo evaluación
     return model
 
-def R2(clf):
-    # Start figure for 1 row and 3 columns
-    y_pred=clf.predict(X_test)
-    # y_pred=np.tile(y_pred, (1, 2))
-    fig, axs = plt.subplots(1, 2, figsize=(18, 6))
+# def R2(clf):
+#     # Start figure for 1 row and 3 columns
+#     y_pred=clf.predict(X_test)
+#     # y_pred=np.tile(y_pred, (1, 2))
+#     fig, axs = plt.subplots(1, 2, figsize=(18, 6))
 
-    # Plot 1
-    r2_1 = r2_score(y_test, y_pred)
-    axs[0].scatter(y_test, y_pred, alpha=0.5)
-    axs[0].plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r-', label=f'R² = {r2_1:.2f}')
-    axs[0].set_title('Valores esacalados: etiqueta verdadera vs estimada')
-    axs[0].set_xlabel('Etiqueta verdadera')
-    axs[0].set_ylabel('Etiqueta estimada')
-    axs[0].legend()
+#     # Plot 1
+#     r2_1 = r2_score(y_test, y_pred)
+#     axs[0].scatter(y_test, y_pred, alpha=0.5)
+#     axs[0].plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r-', label=f'R² = {r2_1:.2f}')
+#     axs[0].set_title('Valores esacalados: etiqueta verdadera vs estimada')
+#     axs[0].set_xlabel('Etiqueta verdadera')
+#     axs[0].set_ylabel('Etiqueta estimada')
+#     axs[0].legend()
 
-    # Concatenar X_test con y_pred para hacer la transformación inversa
-    data_pred = np.concatenate((X_test, y_pred.reshape(-1, 1)), axis=1)
-    data_test = np.concatenate((X_test, y_test.reshape(-1, 1)), axis=1)
+#     # Concatenar X_test con y_pred para hacer la transformación inversa
+#     data_pred = np.concatenate((X_test, y_pred.reshape(-1, 1)), axis=1)
+#     data_test = np.concatenate((X_test, y_test.reshape(-1, 1)), axis=1)
 
-    # Aplicar inverse_transform a toda la matriz
-    data_pred_inv = scaler.inverse_transform(data_pred)
-    data_test_inv = scaler.inverse_transform(data_test)
+#     # Aplicar inverse_transform a toda la matriz
+#     data_pred_inv = scaler.inverse_transform(data_pred)
+#     data_test_inv = scaler.inverse_transform(data_test)
 
-    # Extraer solo las columnas de y después de la transformación inversa
-    y_pred = data_pred_inv[:, -1]  # Última columna
-    y_test_inv = data_test_inv[:, -1]  # Última columna
+#     # Extraer solo las columnas de y después de la transformación inversa
+#     y_pred = data_pred_inv[:, -1]  # Última columna
+#     y_test_inv = data_test_inv[:, -1]  # Última columna
 
-    # Plot 2
-    r2_2 = r2_score(y_test_inv, y_pred)
-    axs[1].scatter(y_test_inv, y_pred, alpha=0.5)
-    axs[1].plot([y_test_inv.min(), y_test_inv.max()], [y_test_inv.min(), y_test_inv.max()], 'r-', label=f'R² = {r2_2:.2f}')
-    axs[1].set_title('Valores sin esacalar: etiqueta verdadera vs estimada')
-    axs[1].set_xlabel('Etiqueta verdadera (COP/kWh)')
-    axs[1].set_ylabel('Etiqueta estimada (COP/kWh)')
-    axs[1].legend()
-    st.pyplot(fig)
+#     # Plot 2
+#     r2_2 = r2_score(y_test_inv, y_pred)
+#     axs[1].scatter(y_test_inv, y_pred, alpha=0.5)
+#     axs[1].plot([y_test_inv.min(), y_test_inv.max()], [y_test_inv.min(), y_test_inv.max()], 'r-', label=f'R² = {r2_2:.2f}')
+#     axs[1].set_title('Valores sin esacalar: etiqueta verdadera vs estimada')
+#     axs[1].set_xlabel('Etiqueta verdadera (COP/kWh)')
+#     axs[1].set_ylabel('Etiqueta estimada (COP/kWh)')
+#     axs[1].legend()
+#     st.pyplot(fig)
 
 
 def mostrar():
@@ -81,6 +81,7 @@ def mostrar():
     st.image("Datos/Imagenes/tabnet.jpg", caption="Arquitectura de la TabNet", use_container_width=True)
 
     clf = load_model()
+    st.write("Modelo cargado")
 
 
   
